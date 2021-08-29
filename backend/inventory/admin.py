85efe1fdec3f82c8609d75de79dc92b4d55e02c9
mouad benbrik <benbrikmouad@outlook.com>
2021-08-29 12:21:42 +0100

    Revert "init"
    This reverts commit 469cd78764a5505e2334e5672833b0fc7e41aa7b.

--- backend/inventory/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-admin.site.register(Machine)
-admin.site.register(Model)
-admin.site.register(Os)
--- backend/inventory/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import *
-
-router = DefaultRouter()
-router.register(r'model', ModelViewSet, basename='model')
-router.register(r'machine', MachineViewSet, basename='machine')
-router.register(r'os', OsViewset, basename='os')
-urlpatterns = router.urls
